749e6be5da65abb7cd6bcaeafcad68d37db64dda
Alejandro <nestorale.salguero@outlook.es>
2023-12-07 00:47:12 -0300

    iniciando proyecto

---
+++ src/main/java/com/example/consultorioBack/ConsultorioBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultorioBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consultorioBack/ConsultorioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultorioBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
